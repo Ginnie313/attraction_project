7a8798b357af6e3b98e7d1fabc6107c8ac49343b
Ginnie <virginiawhite313@gmail.com>
2019-12-13 17:56:33 -0600

    Made the name search inclusive and implemented it on almost every page

--- attractions/views.py
+++ attractions/views.py
@@ -25,12 +25,13 @@ def home(request):
     if 'search' in request.GET:
         name =  request.GET.get('search')
         try:
-            attraction_list = Attraction.objects.filter(attraction_name=name)
+            #Here the icontains gets any attraction with the text in the name
+            attraction_list = Attraction.objects.filter(attraction_name__icontains=name)
             print(attraction)
             #working okay up to here.need to add ability to search for part of name?
             return render(request,"results.html",{"Results":attraction_list})
         except:
-            print("Did not find the attraction")
+            print("Some sort of error occured")
             return render(request, 'home.html')
     return render(request,'home.html')
 
@@ -55,39 +56,160 @@ def advanced_search(request):
     return render(request, "advanced_search.html", {'form': form})
 
 def about(request):
+    if 'search' in request.GET:
+        name =  request.GET.get('search')
+        try:
+            #Here the icontains gets any attraction with the text in the name
+            attraction_list = Attraction.objects.filter(attraction_name__icontains=name)
+            print(attraction)
+            #working okay up to here.need to add ability to search for part of name?
+            return render(request,"results.html",{"Results":attraction_list})
+        except:
+            print("Some sort of error occured")
+            return render(request, 'home.html')
     return render(request, "about.html")
 
 def results(request):
+    if 'search' in request.GET:
+        name =  request.GET.get('search')
+        try:
+            #Here the icontains gets any attraction with the text in the name
+            attraction_list = Attraction.objects.filter(attraction_name__icontains=name)
+            print(attraction)
+            #working okay up to here.need to add ability to search for part of name?
+            return render(request,"results.html",{"Results":attraction_list})
+        except:
+            print("Some sort of error occured")
+            return render(request, 'home.html')
     return render(request, "results.html", {"Results":results})
 
 def attraction(request, attraction_name):
+    if 'search' in request.GET:
+        name =  request.GET.get('search')
+        try:
+            #Here the icontains gets any attraction with the text in the name
+            attraction_list = Attraction.objects.filter(attraction_name__icontains=name)
+            print(attraction)
+            #working okay up to here.need to add ability to search for part of name?
+            return render(request,"results.html",{"Results":attraction_list})
+        except:
+            print("Some sort of error occured")
+            return render(request, 'home.html')
     attraction = Attraction.objects.get(attraction_name=attraction_name)
     return render(request, "attraction.html", {"Attraction":attraction})
 
 def magic_kingdom(request):
+    if 'search' in request.GET:
+        name =  request.GET.get('search')
+        try:
+            #Here the icontains gets any attraction with the text in the name
+            attraction_list = Attraction.objects.filter(attraction_name__icontains=name)
+            print(attraction)
+            #working okay up to here.need to add ability to search for part of name?
+            return render(request,"results.html",{"Results":attraction_list})
+        except:
+            print("Some sort of error occured")
+            return render(request, 'home.html')
     attraction_list = Attraction.objects.filter(park="Magic Kingdom")
     return render(request, "magic_kingdom.html", {"Attractions": attraction_list})
 
 def about_mk(request):
+    if 'search' in request.GET:
+        name =  request.GET.get('search')
+        try:
+            #Here the icontains gets any attraction with the text in the name
+            attraction_list = Attraction.objects.filter(attraction_name__icontains=name)
+            print(attraction)
+            #working okay up to here.need to add ability to search for part of name?
+            return render(request,"results.html",{"Results":attraction_list})
+        except:
+            print("Some sort of error occured")
+            return render(request, 'home.html')
     return render(request, "about_mk.html")
 
 def epcot(request):
+    if 'search' in request.GET:
+        name =  request.GET.get('search')
+        try:
+            #Here the icontains gets any attraction with the text in the name
+            attraction_list = Attraction.objects.filter(attraction_name__icontains=name)
+            print(attraction)
+            #working okay up to here.need to add ability to search for part of name?
+            return render(request,"results.html",{"Results":attraction_list})
+        except:
+            print("Some sort of error occured")
+            return render(request, 'home.html')
     attraction_list = Attraction.objects.filter(park="Epcot")
     return render(request, "epcot.html", {"Attractions": attraction_list})
 
 def about_ep(request):
+    if 'search' in request.GET:
+        name =  request.GET.get('search')
+        try:
+            #Here the icontains gets any attraction with the text in the name
+            attraction_list = Attraction.objects.filter(attraction_name__icontains=name)
+            print(attraction)
+            #working okay up to here.need to add ability to search for part of name?
+            return render(request,"results.html",{"Results":attraction_list})
+        except:
+            print("Some sort of error occured")
+            return render(request, 'home.html')
     return render(request, "about_ep.html")
 
 def hollywood_studios(request):
+    if 'search' in request.GET:
+        name =  request.GET.get('search')
+        try:
+            #Here the icontains gets any attraction with the text in the name
+            attraction_list = Attraction.objects.filter(attraction_name__icontains=name)
+            print(attraction)
+            #working okay up to here.need to add ability to search for part of name?
+            return render(request,"results.html",{"Results":attraction_list})
+        except:
+            print("Some sort of error occured")
+            return render(request, 'home.html')
     attraction_list = Attraction.objects.filter(park="Hollywood Studios")
     return render(request, "hollywood_studios.html", {"Attractions": attraction_list})
 
 def about_hs(request):
+    if 'search' in request.GET:
+        name =  request.GET.get('search')
+        try:
+            #Here the icontains gets any attraction with the text in the name
+            attraction_list = Attraction.objects.filter(attraction_name__icontains=name)
+            print(attraction)
+            #working okay up to here.need to add ability to search for part of name?
+            return render(request,"results.html",{"Results":attraction_list})
+        except:
+            print("Some sort of error occured")
+            return render(request, 'home.html')
     return render(request, "about_hs.html")
 
 def animal_kingdom(request):
+    if 'search' in request.GET:
+        name =  request.GET.get('search')
+        try:
+            #Here the icontains gets any attraction with the text in the name
+            attraction_list = Attraction.objects.filter(attraction_name__icontains=name)
+            print(attraction)
+            #working okay up to here.need to add ability to search for part of name?
+            return render(request,"results.html",{"Results":attraction_list})
+        except:
+            print("Some sort of error occured")
+            return render(request, 'home.html')
     attraction_list = Attraction.objects.filter(park="Animal Kingdom")
     return render(request, "animal_kingdom.html", {"Attractions": attraction_list})
 
 def about_ank(request):
+    if 'search' in request.GET:
+        name =  request.GET.get('search')
+        try:
+            #Here the icontains gets any attraction with the text in the name
+            attraction_list = Attraction.objects.filter(attraction_name__icontains=name)
+            print(attraction)
+            #working okay up to here.need to add ability to search for part of name?
+            return render(request,"results.html",{"Results":attraction_list})
+        except:
+            print("Some sort of error occured")
+            return render(request, 'home.html')
     return render(request, "about_ank.html")
